8bf0a233a73d0b46f55808bd6c96db4abb758312
Kang Jisoon <wltnskjs@naver.com>
2024-05-24 22:45:36 +0900

    🚚 Rename Project

---
+++ SwiftUI-Calc/SwiftUI-Calc/SwiftUI_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalcApp.swift
+//  SwiftUI-Calc
+//
+//  Created by 강지순 on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
