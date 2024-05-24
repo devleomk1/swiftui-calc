8fce56286c9a79a77c2d106107a8523882320286
Kang Jisoon <wltnskjs@naver.com>
2024-05-24 22:36:41 +0900

    🎉 Make Xcode Project

---
+++ swift-calc/swift-calc/swift_calcApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_calcApp.swift
+//  swift-calc
+//
+//  Created by 강지순 on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_calcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
